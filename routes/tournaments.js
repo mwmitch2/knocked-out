e0a4189db06e518674e8c013a3de80c692f27519
uncgsenih <111514786+uncgsenih@users.noreply.github.com>
2022-10-21 17:03:46 -0400

    Update tournaments.js

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -7,6 +7,27 @@ router.get('/', function(req, res) {
     res.render('tournaments/index')
 })
 
-// Handle new tournament POST request
+// Handle POST request to create new tournament
+router.post('/new', function(req, res) {
+    // get data from form and add to tournament array
+    let name = req.body.name;
+    let size = req.body.size;
+    var type = req.body.type;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+
+    let newTournament = {name: name, size: size, type: type, author: author}
+    
+    Tournament.create(newTournament, function(err, newlyCreated) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/tournaments')
+        }
+    })
+})
+
 
 module.exports = router
